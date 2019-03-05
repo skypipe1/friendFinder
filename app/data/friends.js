3cca059a457e561c1ad60b5d093c219b3b0e99ca
Piper <skylarpiper710@gmail.com>
2019-03-05 14:05:22 -0600

    inital files made htmls made and built out as well as the friends folder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,84 @@
+var friends = [
+{
+  "name":"Homer Simpson",
+  "photo":"https://i.imgur.com/y9dH2bJ.jpg",
+  "scores":[
+      5,
+      2,
+      5,
+      1,
+      1,
+      1,
+      5,
+      1,
+      1,
+      2
+    ]
+},
+{
+  "name":"Marge Simpson",
+  "photo":"https://i.imgur.com/H6mZGTJ.png",
+  "scores":[
+      2,
+      2,
+      1,
+      2,
+      3,
+      3,
+      3,
+      3,
+      5,
+      4
+    ]
+},
+{
+  "name":"Bart Simpson",
+  "photo":"https://i.imgur.com/WE0hScq.png",
+  "scores":[
+      4,
+      3,
+      4,
+      5,
+      1,
+      5,
+      2,
+      5,
+      2,
+      1
+    ]
+},
+{
+  "name":"Lisa Simpson",
+  "photo":"https://i.imgur.com/A0boDiy.jpg",
+  "scores":[
+      1,
+      5,
+      3,
+      4,
+      5,
+      2,
+      5,
+      2,
+      5,
+      5
+    ]
+},
+{
+  "name":"Ned Flanders",
+  "photo":"https://i.imgur.com/R2OR40Y.png",
+  "scores":[
+      2,
+      1,
+      1,
+      2,
+      4,
+      4,
+      5,
+      1,
+      4,
+      5
+    ]
+},
+
+];
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,3 @@
 
+// GET route to /survey which should display on server page
+// A default, catch-all route that leads to `home.html` which displays the home page.
